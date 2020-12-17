c497139b25932179ea982cbe7b75f0d6370c3d25
Gabriel <gabrielsantosrv@outlook.com>
2020-12-17 11:32:18 -0300

    agents

--- QLearningAgent.py
+++ QLearningAgent.py
@@ -1,38 +1,39 @@
 import numpy as np
 from Agent import Agent
 
-class QLearningAgent(Agent):
-def __init__(self, epsilon, alpha, gamma, num_state, num_actions, action_space):
-    """
-    Contructor
-    Args:
-        epsilon: The degree of exploration
-        gamma: The discount factor
-        num_state: The number of states
-        num_actions: The number of actions
-        action_space: To call the random action
-    """
-    self.epsilon = epsilon
-    self.alpha = alpha
-    self.gamma = gamma
-    self.num_state = num_state
-    self.num_actions = num_actions
 
-    self.Q = np.zeros((self.num_state, self.num_actions))
-    self.action_space = action_space
-def update(self, state, state2, reward, action, action2):
-    """
-    Update the action value function using the Q-Learning update.
-    Q(S, A) = Q(S, A) + alpha(reward + (gamma * Q(S_, A_) - Q(S, A))
-    Args:
-        prev_state: The previous state
-        next_state: The next state
-        reward: The reward for taking the respective action
-        prev_action: The previous action
-        next_action: The next action
-    Returns:
-        None
-    """
-    predict = self.Q[state, action]
-    target = reward + self.gamma * np.max(self.Q[state2, :])
-    self.Q[state, action] += self.alpha * (target - predict)
+class QLearningAgent(Agent):
+    def __init__(self, epsilon, alpha, gamma, num_state, num_actions, action_space):
+        """
+        Contructor
+        Args:
+            epsilon: The degree of exploration
+            gamma: The discount factor
+            num_state: The number of states
+            num_actions: The number of actions
+            action_space: To call the random action
+        """
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.gamma = gamma
+        self.num_state = num_state
+        self.num_actions = num_actions
+
+        self.Q = np.zeros((self.num_state, self.num_actions))
+        self.action_space = action_space
+
+    def update(self, prev_state, next_state, reward, prev_action):
+        """
+        Update the action value function using the Q-Learning update.
+        Q(S_t, A_t) = Q(S_t, A_t) + alpha(reward + (gamma * Max Q(S_t+1, *) - Q(S_t, A_t))
+        Args:
+            prev_state: The previous state
+            next_state: The next state
+            reward: The reward for taking the respective action
+            prev_action: The previous action
+        Returns:
+            None
+        """
+        predict = self.Q[prev_state, prev_action]
+        target = reward + self.gamma * np.max(self.Q[next_state, :])
+        self.Q[prev_state, prev_action] += self.alpha * (target - predict)
